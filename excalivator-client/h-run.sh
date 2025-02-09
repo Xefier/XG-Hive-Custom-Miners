3c571c5aca076379287ea3526ce36d972602be34
Xefier <admin@xefiergames.com>
2025-02-09 15:36:02 -0600

    Added cargo install to excalivator-client

--- excalivator-client/h-run.sh
+++ excalivator-client/h-run.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-cargo install excalivator-client
+# Check if cargo is installed
+if command -v cargo &> /dev/null; then
+    echo "Cargo is already installed."
+else
+    echo "Cargo is not installed. Installing..."
+    sudo apt-get update && \
+    sudo apt install -y curl git build-essential libssl-dev pkg-config && \
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh -s -- -y && \
+    echo "Installation complete. Rebooting..." && \
+    sudo reboot
+fi
+
+# Check if excalivator-client is installed
+if cargo install --list | grep -q "excalivator-client"; then
+    echo "Excalivator-client is already installed."
+else
+    echo "Excalivator-client is not installed. Installing..."
+    cargo install excalivator-client
+fi
 
 . h-manifest.conf
 if [[ ! -f $CUSTOM_CONFIG_FILENAME ]]; then
