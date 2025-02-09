187aa46c83207ae6fb10258dfa66ebe5b0d7f13c
Xefier <admin@xefiergames.com>
2025-02-09 14:02:05 -0600

    Added missing variables to h-manifest.conf and added error if config is not generated

--- coal_pool_client/h-run.sh
+++ coal_pool_client/h-run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 . h-manifest.conf
+if [[ ! -f $CUSTOM_CONFIG_FILENAME ]]; then
+    echo "Configuration file $CUSTOM_CONFIG_FILENAME not found. Exiting..."
+    exit 1
+fi
 . $CUSTOM_CONFIG_FILENAME
 
 #coal-pool-client
