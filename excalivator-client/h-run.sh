a6f6eee2212a14f8f9f43cb604531d6dddd7f32b
Xefier <admin@xefiergames.com>
2025-02-09 15:11:28 -0600

    Changed excalivator-client to use cargo install instead

--- excalivator-client/h-run.sh
+++ excalivator-client/h-run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# Install GLIBC_2.39 if necessary
-ldd --version | grep -q "2.39" && echo "GLIBC 2.39 is already installed." || (sudo apt update && sudo apt install -y build-essential manpages-dev gawk bison && cd /usr/src && sudo wget -q http://ftp.gnu.org/gnu/libc/glibc-2.39.tar.gz && sudo tar -xzf glibc-2.39.tar.gz && cd glibc-2.39 && mkdir -p build && cd build && sudo ../configure --prefix=/opt/glibc-2.39 && sudo make -j$(nproc) && sudo make install && echo "export LD_LIBRARY_PATH=/opt/glibc-2.39/lib:\$LD_LIBRARY_PATH" | sudo tee -a /etc/profile && source /etc/profile && echo "GLIBC 2.39 installed successfully.")
+cargo install excalivator-client
 
 . h-manifest.conf
 if [[ ! -f $CUSTOM_CONFIG_FILENAME ]]; then
@@ -10,4 +9,4 @@ if [[ ! -f $CUSTOM_CONFIG_FILENAME ]]; then
 fi
 . $CUSTOM_CONFIG_FILENAME
 
-./excalivator-client mine-public-key --pubkey $TEMPLATE
+/root/.cargo/bin/excalivator-client mine-public-key --pubkey $TEMPLATE
