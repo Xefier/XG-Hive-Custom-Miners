1707184072284bce80b3a3d6e4c6cc8675cb00b8
Xefier <admin@xefiergames.com>
2025-02-09 10:34:15 -0600

    Added TEMPLATE/PASS to coal_pool_client

--- coal_pool_client/h-config.sh
+++ coal_pool_client/h-config.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+# Initialize configuration
+conf=""
+
+# Add CUSTOM_TEMPLATE if available
+if [[ ! -z $CUSTOM_TEMPLATE ]]; then
+    conf+="TEMPLATE="
+    conf+=$CUSTOM_TEMPLATE
+    conf+="\n"
+fi
+
+# Add CUSTOM_PASS if available
+if [[ ! -z $CUSTOM_PASS ]]; then
+    conf+="PASS="
+    conf+=$CUSTOM_PASS
+    conf+="\n"
+fi
+
+# Output configuration to the expected config file
+echo -e "$conf" > $CUSTOM_CONFIG_FILENAME
--- coal_pool_client/h-run.sh
+++ coal_pool_client/h-run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+. h-manifest.conf
+. $CUSTOM_CONFIG_FILENAME
+
 #coal-pool-client
-/root/.cargo/bin/coal-pool-client
+/root/.cargo/bin/coal-pool-client --pubkey $TEMPLATE
